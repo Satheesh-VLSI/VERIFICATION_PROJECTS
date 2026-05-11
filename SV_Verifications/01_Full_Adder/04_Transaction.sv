4e18209f634d3ee752def96aaee5fae5d74c0d85
Satheesh <satheeshnarayan05@gmail.com>
2026-05-11 15:05:36 +0530

    Update 04_Transaction.sv

--- SV_Verifications/01_Full_Adder/04_Transaction.sv
+++ SV_Verifications/01_Full_Adder/04_Transaction.sv
@@ -1,1 +1,13 @@
 
+//Transaction
+class Transaction;
+  rand bit a,b,cin;
+  bit sum,cout;
+  
+  
+  function void display(string name);
+    $display("[%s] a=%b b=%b cin=%b | sum=%b cout=%b",name,a,b,cin,sum,cout);
+  endfunction
+  
+endclass:Transaction
+
