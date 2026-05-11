506a676611a7aa7c025b0461259de9c10907dc33
Satheesh <satheeshnarayan05@gmail.com>
2026-05-11 15:02:59 +0530

    Update 02_Testbench_top.sv

--- SV_Verifications/01_Full_Adder/02_Testbench_top.sv
+++ SV_Verifications/01_Full_Adder/02_Testbench_top.sv
@@ -1,1 +1,33 @@
 
+
+
+module tb_top;
+  
+  logic clk;
+  initial clk=0;
+  always #5 clk=~clk;
+  
+  //INTERFACE Instantiation
+  FA_INTERTFACE inf(clk);
+  
+  
+  //DUT instantiation
+  full_adder fa1(.a(inf.a),
+                 .b(inf.b),
+                 .cin(inf.cin),
+                 .sum(inf.sum),
+                 .carry(inf.cout));
+  
+    Environment env;
+  
+  initial begin 
+     #20;
+    env=new(inf);
+    env.test();
+    #20;
+    $finish;
+    
+  end 
+  
+  
+endmodule:tb_top
