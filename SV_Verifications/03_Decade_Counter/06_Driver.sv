2d0ca7864226fb84b6f24470912dca317ab4c965
Satheesh <satheeshnarayan05@gmail.com>
2026-05-13 13:49:36 +0530

    Update 06_Driver.sv

--- SV_Verifications/03_Decade_Counter/06_Driver.sv
+++ SV_Verifications/03_Decade_Counter/06_Driver.sv
@@ -1,29 +1,25 @@
-//DRIVER
+
+//===========================
+//        DRIVER 
+//===========================
 class Driver;
-  virtual FA_INTERTFACE inf;
+  
+  virtual DECADE_CNT_INTERFACE inf;
   mailbox #(Transaction) gen2drv;
-  
-  
-  function new(virtual FA_INTERTFACE inf,mailbox #(Transaction) gen2drv);
-    this.inf=inf;
+
+  function new(virtual DECADE_CNT_INTERFACE inf,mailbox #(Transaction) gen2drv);
+    this.inf= inf;
     this.gen2drv=gen2drv;
   endfunction
-  
-  task main();
-    forever begin
-      
-      Transaction trans;
-      
-      gen2drv.get(trans);
-      @(posedge inf.clk);
-      
-      inf.a=trans.a;
-      inf.b=trans.b;
-      inf.cin=trans.cin;
-      
-      
-    end 
-  endtask
-  
-endclass:Driver
 
+  task main();
+    inf.rst=1;
+    forever begin
+      Transaction trans;
+      gen2drv.get(trans);
+
+      @(inf.cb);
+      inf.cb.rst<=trans.rst;
+    end
+  endtask
+endclass:Driver
