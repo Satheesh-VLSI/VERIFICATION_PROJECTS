5b727d428a2c3fdeaf9953a41445dc14ed1da034
Satheesh <satheeshnarayan05@gmail.com>
2026-05-13 13:44:39 +0530

    Update 03_Environment.sv

--- SV_Verifications/03_Decade_Counter/03_Environment.sv
+++ SV_Verifications/03_Decade_Counter/03_Environment.sv
@@ -1,15 +1,19 @@
 
-//ENVIRONMENT
+//========================
+//      ENVIRONMENT
+//========================
 class Environment;
+  
   Generator  gen;
   Driver     drv;
   Monitor    mon;
   Scoreboard scr;
   
-  mailbox #(Transaction) gen2drv;
-  mailbox #(Transaction) mon2scr;
-  
-  function new(virtual FA_INTERTFACE inf);
+  mailbox #(Transaction) gen2drv;  //mailbox to send data from generator to driver
+  mailbox #(Transaction) mon2scr;  //mailbox to send data from monitor to scoreboard
+
+  function new(virtual DECADE_CNT_INTERFACE inf);
+    
     gen2drv=new();
     mon2scr=new();
     
@@ -17,16 +21,19 @@ class Environment;
     drv=new(inf,gen2drv);
     mon=new(inf,mon2scr);
     scr=new(mon2scr);
+    
   endfunction
-  
-  
+
   task test();
+    
     fork
       gen.main();
       drv.main();
       mon.main();
       scr.main();
     join_none
-    wait(scr.done.triggered) disable fork;
+    
+    wait(scr.done.triggered);
+    disable fork;
   endtask
-endclass:Environment
+endclass
