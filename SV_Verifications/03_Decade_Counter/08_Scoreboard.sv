bc328add7198ebd87fcc9ee7adfe47e27bcedcf6
Satheesh <satheeshnarayan05@gmail.com>
2026-05-13 13:48:15 +0530

    Update 08_Scoreboard.sv

--- SV_Verifications/03_Decade_Counter/08_Scoreboard.sv
+++ SV_Verifications/03_Decade_Counter/08_Scoreboard.sv
@@ -1,53 +1,68 @@
-
-//SCOREBOARD
+//===========================
+//       SCOREBOARD 
+//===========================
 class Scoreboard;
-  
   mailbox #(Transaction) mon2scr;
   
-  bit expected_sum,expected_cout;
+  bit [3:0] expected_q;
+  bit prev_rst=1;
   
-  
+  int count;
+  int error_cnt;
   event done;
-  integer count=0;
-  
-  
+
   function new(mailbox #(Transaction) mon2scr);
-    
-    this.mon2scr=mon2scr;
+    this.mon2scr = mon2scr;
   endfunction
-  
-    int error_cnt;
+
   task main();
-    error_cnt=0;
     forever begin
-      
       Transaction trans;
+      mon2scr.get(trans);
+
       
-      mon2scr.get(trans); //GETTING THE TRANS OBJECT FROM THE MONITOR USING MAILBOX 
-      
-      expected_sum=trans.a^trans.b^trans.cin;
-      expected_cout=(trans.a & trans.b)|(trans.b &trans.cin)|(trans.a&trans.cin);
-      
-      if(expected_sum!=trans.sum || expected_cout!=trans.cout)
-        error_cnt++;
-      
-      count++;
-      if(count==100)begin 
-        ->>done;
-        verdict();
+      if(prev_rst) begin 
+        expected_q = 0;
+      end else begin
+        if(expected_q != 9)
+          expected_q = expected_q + 1;
+        else
+          expected_q = 0;
       end
-    end 
+
+      // Compare
+      if(expected_q != trans.q) begin
+        error_cnt++;
+        $display("--------------------------------");
+        $display("COUNTER FAILED at Time %0t", $time);
+        $display("Expected Q = %0d", expected_q);
+        $display("Actual   Q = %0d", trans.q);
+        $display("--------------------------------");
+      end
+      
+      prev_rst=trans.rst; 
+
+      count++;
+      if(count == 100) begin
+        verdict();
+        -> done;
+        break;
+      end
+    end
   endtask
-  
+
   task verdict();
-    $display("==================================");
-    $display("DESIGN STATUS:");
-    if(error_cnt==0)
-      $display("    Full Adder PASSED");
-    else
-      $display("    Full Adder FAILED");
+    $display("\n===================================");
+    $display("        DESIGN STATUS");
+    $display("===================================");
     
-    $display("==================================");
+
+    if(error_cnt == 0) 
+      $display("DECADE COUNTER VERIFICATION PASSED");
+    else               
+      $display("DECADE COUNTER VERIFICATION FAILED");
+      
+    $display("Total Errors = %0d", error_cnt);
+    $display("===================================\n");
   endtask
-  
-endclass:Scoreboard
+endclass
