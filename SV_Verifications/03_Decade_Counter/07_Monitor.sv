0c1f33a482f1b17be418140b93fe5b0570a04f7e
Satheesh <satheeshnarayan05@gmail.com>
2026-05-13 13:48:56 +0530

    Update 07_Monitor.sv

--- SV_Verifications/03_Decade_Counter/07_Monitor.sv
+++ SV_Verifications/03_Decade_Counter/07_Monitor.sv
@@ -1,32 +1,29 @@
-//MONITOR
+//==========================
+//        MONITOR 
+//==========================
 class Monitor;
-  Transaction trans;
   
-  virtual FA_INTERTFACE inf;
+  virtual DECADE_CNT_INTERFACE inf;
   mailbox #(Transaction) mon2scr;
-  
-  
-  function new(virtual FA_INTERTFACE inf,mailbox #(Transaction) mon2scr);
+
+  function new(virtual DECADE_CNT_INTERFACE inf,mailbox #(Transaction) mon2scr);
     this.inf=inf;
     this.mon2scr=mon2scr;
   endfunction
-  
+
   task main();
-    
-    forever begin 
-      trans=new();
-      @(posedge inf.clk);
-      #1;
-      trans.a=inf.a;
-      trans.b=inf.b;
-      trans.cin=inf.cin;
-      trans.sum=inf.sum;
-      trans.cout=inf.cout;
+    forever begin
       
+      Transaction trans;
+      trans=new();
+
+      @(inf.cb);
+      
+      trans.rst=inf.rst;
+      trans.q=inf.cb.q;
+
       mon2scr.put(trans);
       trans.display("MONITOR");
     end
   endtask
-  
-endclass:Monitor
-
+endclass
