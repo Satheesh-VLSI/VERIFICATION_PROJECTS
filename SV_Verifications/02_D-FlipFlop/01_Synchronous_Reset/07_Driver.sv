486744f1960752e6cdf04e39ea568cc9c133539b
Satheesh <satheeshnarayan05@gmail.com>
2026-05-16 10:37:00 +0530

    Update 07_Driver.sv

--- SV_Verifications/02_D-FlipFlop/01_Synchronous_Reset/07_Driver.sv
+++ SV_Verifications/02_D-FlipFlop/01_Synchronous_Reset/07_Driver.sv
@@ -1,1 +1,27 @@
+//===========================
+//        DRIVER 
+//===========================
+class Driver;
+  
+  virtual DFF_INTERFACE inf;
+  mailbox #(Transaction) gen2drv;
 
+  function new(virtual DFF_INTERFACE inf,mailbox #(Transaction) gen2drv);
+    this.inf= inf;
+    this.gen2drv=gen2drv;
+  endfunction
+
+  task main();
+    forever begin
+      Transaction trans;
+      gen2drv.get(trans);
+
+      @(inf.cb);
+
+      inf.cb.d<=trans.d;
+      inf.cb.rst<=trans.rst;
+      
+      
+    end
+  endtask
+endclass:Driver
