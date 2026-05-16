31ccbaa1dcc7d407e61aeefc8f601354b63a69af
Satheesh <satheeshnarayan05@gmail.com>
2026-05-16 10:30:42 +0530

    Create TRANSACTION.sv

---
+++ SV_Verifications/02_D-FlipFlop/01_Synchronous_Reset/TRANSACTION.sv
@@ -0,0 +1,16 @@
+
+
+//===============================
+//         TRANSACTION
+//===============================
+class Transaction;
+  rand bit d;
+  rand bit rst;
+  bit q;
+  
+  constraint rst_c{ rst dist {0:=9,1:=2};}
+  
+  function void display(string name);
+    $display("[%s] d=%0b rst=%0b q=%0b",name,d,rst,q);
+  endfunction
+endclass
