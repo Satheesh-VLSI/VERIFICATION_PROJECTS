3717f8677255df00cb61d0916f1612e2792f92ce
Satheesh <satheeshnarayan05@gmail.com>
2026-05-17 05:36:13 +0530

    Update 08_Monitor.sv

--- SV_Verifications/02_D-FlipFlop/01_Synchronous_Reset/02_Asynchronous_Reset/08_Monitor.sv
+++ SV_Verifications/02_D-FlipFlop/01_Synchronous_Reset/02_Asynchronous_Reset/08_Monitor.sv
@@ -1,1 +1,41 @@
+//========================
+//        MONITOR 
+//========================
+class Monitor;
+  virtual DFF_INTERFACE inf;
+  mailbox #(Transaction) mon2scr;
+
+  function new(virtual DFF_INTERFACE inf,mailbox #(Transaction) mon2scr);
+    this.inf=inf;
+    this.mon2scr=mon2scr;
+  endfunction
+
+  task main();
+    forever begin
+      Transaction trans=new();
+
+      @(inf.cb1 or posedge inf.rst);
+
+      
+      trans.rst=inf.rst; 
+
+      if(inf.rst) begin
+        trans.Edge=0;
+        #1;
+        trans.q=inf.q;
+      end 
+      else begin
+        trans.Edge=1;
+        trans.d=inf.d; 
+        trans.q=inf.cb1.q;
+      end
+      mon2scr.put(trans);
+      
+      trans.display("MONITOR");
+    end
+  endtask
+endclass:Monitor
+
+
+
 
