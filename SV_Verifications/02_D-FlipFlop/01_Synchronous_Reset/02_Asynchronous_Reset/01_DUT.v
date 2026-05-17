a2c9d5e15d8b5805ace5b81d04e130e35bbd492f
Satheesh <satheeshnarayan05@gmail.com>
2026-05-17 05:38:49 +0530

    Update 01_DUT.v

--- SV_Verifications/02_D-FlipFlop/01_Synchronous_Reset/02_Asynchronous_Reset/01_DUT.v
+++ SV_Verifications/02_D-FlipFlop/01_Synchronous_Reset/02_Asynchronous_Reset/01_DUT.v
@@ -1,1 +1,16 @@
-
+//=================
+//      DUT 
+//=================
+module d_ff(
+  input  logic d,
+  input  logic clk,
+  input  logic rst,
+  output logic q
+);
+  always_ff @(posedge clk or posedge rst) begin
+    if(rst) 
+      q <=0;
+    else    
+      q <= d;
+  end
+endmodule
