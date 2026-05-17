9eff5515135b9dbdd966e2d69c0ab9cb6c80a308
Satheesh <satheeshnarayan05@gmail.com>
2026-05-17 05:35:35 +0530

    Update 07_Driver.sv

--- SV_Verifications/02_D-FlipFlop/01_Synchronous_Reset/02_Asynchronous_Reset/07_Driver.sv
+++ SV_Verifications/02_D-FlipFlop/01_Synchronous_Reset/02_Asynchronous_Reset/07_Driver.sv
@@ -1,1 +1,41 @@
+//=======================
+//        DRIVER
+//=======================
+class Driver;
+  
+  virtual DFF_INTERFACE inf;
+  mailbox #(Transaction) gen2drv;
+
+  function new(virtual DFF_INTERFACE inf,mailbox #(Transaction) gen2drv);
+    this.inf=inf;
+    this.gen2drv=gen2drv;
+  endfunction
+
+  task main();
+    
+    forever begin
+      Transaction trans;
+      gen2drv.get(trans);
+
+      @(inf.cb);
+      inf.cb.d<=trans.d;
+
+      fork
+        begin
+          #($urandom_range(1,4)); 
+          inf.rst=trans.rst; 
+          
+          //Force reset to disappear before next clock(just fro testing)
+          if(trans.rst==1) begin
+            #($urandom_range(1,4));
+            inf.rst=0; 
+          end
+        end
+      join
+    end
+  endtask
+endclass:Driver
+
+
+
 
