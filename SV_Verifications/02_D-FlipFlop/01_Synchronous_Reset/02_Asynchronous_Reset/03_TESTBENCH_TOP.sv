d80ff233972bd43d02f4437b33c9bee640434e7e
Satheesh <satheeshnarayan05@gmail.com>
2026-05-17 05:38:09 +0530

    Update 03_TESTBENCH_TOP.sv

--- SV_Verifications/02_D-FlipFlop/01_Synchronous_Reset/02_Asynchronous_Reset/03_TESTBENCH_TOP.sv
+++ SV_Verifications/02_D-FlipFlop/01_Synchronous_Reset/02_Asynchronous_Reset/03_TESTBENCH_TOP.sv
@@ -1,1 +1,31 @@
+//===========================
+//      TESTBENCH TOP
+//===========================
+module tb_top;
+  logic clk;
+  
+  initial clk=0;
+  always #5 clk=~clk;
 
+  DFF_INTERFACE inf(clk);
+
+  d_ff dut(.d(inf.d),
+           .clk(inf.clk),
+           .rst(inf.rst),
+           .q(inf.q));
+
+  Environment env;
+
+  initial begin
+    
+    env=new(inf);
+    
+    $display("============================================");
+    $display("   DFF(ASYNC RESET) VERIFICATION STARTED");
+    $display("============================================");
+    
+    env.test();
+    #20;
+    $finish;
+  end
+endmodule
