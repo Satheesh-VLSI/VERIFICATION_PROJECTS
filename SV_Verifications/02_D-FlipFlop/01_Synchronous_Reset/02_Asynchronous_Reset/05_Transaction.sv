46ef4248f90aa71a696a16b02a5a3f7a2ffff7a0
Satheesh <satheeshnarayan05@gmail.com>
2026-05-17 05:34:26 +0530

    Update 05_Transaction.sv

--- SV_Verifications/02_D-FlipFlop/01_Synchronous_Reset/02_Asynchronous_Reset/05_Transaction.sv
+++ SV_Verifications/02_D-FlipFlop/01_Synchronous_Reset/02_Asynchronous_Reset/05_Transaction.sv
@@ -1,1 +1,13 @@
+//============================
+//        TRANSACTION
+//============================
+class Transaction;
+  rand bit d;
+  rand bit rst;
+  bit q;
+  bit Edge;
 
+  function void display(string name);
+    $display("[%s] d=%0b rst=%0b q=%0b Edge=%0b",name,d,rst,q,Edge);
+  endfunction
+endclass:Transaction
