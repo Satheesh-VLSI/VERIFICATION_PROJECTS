021817975a08b30eb93bb1d908ef278cad75c404
Satheesh <satheeshnarayan05@gmail.com>
2026-05-17 05:33:55 +0530

    Update 02_INTERFACE.sv

--- SV_Verifications/02_D-FlipFlop/01_Synchronous_Reset/02_Asynchronous_Reset/02_INTERFACE.sv
+++ SV_Verifications/02_D-FlipFlop/01_Synchronous_Reset/02_Asynchronous_Reset/02_INTERFACE.sv
@@ -1,1 +1,21 @@
+//==============================
+//     INTERFACE 
+//==============================
+interface DFF_INTERFACE(input logic clk);
+  logic d,rst;
+  logic q;
 
+  clocking cb @(posedge clk);
+    default input #1step output #0;
+    input q;
+    output d;
+  endclocking
+  
+  clocking cb1 @(posedge clk);
+    default input #1step output #0;
+    input q,d;
+  endclocking
+
+  modport DUT(input d,rst,clk,output q);
+  modport TB(clocking cb,output rst);
+endinterface
