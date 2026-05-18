49cd4874f9cc94705e4415cf4d7a6d132748eb6f
Satheesh <satheeshnarayan05@gmail.com>
2026-05-18 09:16:24 +0530

    Update 09_Scoreboard.sv

--- SV_Verifications/04_Dual_Port_RAM/09_Scoreboard.sv
+++ SV_Verifications/04_Dual_Port_RAM/09_Scoreboard.sv
@@ -1,1 +1,141 @@
+//===========================
+//       SCOREBOARD 
+//===========================
+class Scoreboard;
+
+  mailbox #(Transaction) mon2scr;
+
+  bit [7:0] Queue_A[$];
+  bit [7:0] Queue_B[$];
+  reg [7:0] memory [int];
+
+  bit [7:0] exp_A;
+  bit [7:0] exp_B;
+
+  bit [7:0] old_read;
+
+
+  int transaction_cnt;
+  int count;
+  int error_cnt;
+
+  event done;
+
+  bit skip_en;
+
+  function new(mailbox #(Transaction) mon2scr);
+    this.mon2scr=mon2scr;
+  endfunction
+
+  task main();
+
+    forever begin
+      Transaction trans;
+      mon2scr.get(trans);
+
+      if(skip_en) begin
+        skip_en=0;
+        continue;
+      end
+
+      //--------check previous reads--------
+      if(Queue_A.size()>0) begin
+        exp_A=Queue_A.pop_front();
+
+        if(exp_A!=trans.data_out_A) begin
+
+          error_cnt++;
+
+          $display("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX");
+          $display("X    [Time %4t] PORT A FAILED           X",$time);
+          $display("X Expected   :  %3d                      X",exp_A);
+          $display("X Actual     :  %3d                      X",trans.data_out_A);
+          $display("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX");
+        end
+      end
+
+      if(Queue_B.size()>0) begin
+
+        exp_B=Queue_B.pop_front();
+
+        if(exp_B!=trans.data_out_B) begin
+          error_cnt++;
+
+          $display("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX");
+          $display("X    [Time %4t] PORT B FAILED           X",$time);
+          $display("X Expected   :  %3d                      X",exp_B);
+          $display("X Actual     :  %3d                      X",trans.data_out_B);
+          $display("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX");
+        end
+      end
+
+      //-----------Process current write-------------
+      if(trans.addr_A!=trans.addr_B) begin
+        if(trans.mode_A)
+          memory[trans.addr_A]=trans.data_in_A;
+        if(trans.mode_B)
+          memory[trans.addr_B]=trans.data_in_B;
+      end
+      else begin
+        if(trans.mode_A)
+          memory[trans.addr_A]=trans.data_in_A;
+      end
+
+      //----------Process current reads---------------
+      if(trans.addr_A!=trans.addr_B) begin
+        if(!trans.mode_A)
+          Queue_A.push_back(memory[trans.addr_A]);
+
+        if(!trans.mode_B) begin
+          old_read=memory[trans.addr_B];
+          Queue_B.push_back(memory[trans.addr_B]);
+        end
+
+      end
+      //collision scenario
+      else begin
+        if(!trans.mode_A)
+          Queue_A.push_back(memory[trans.addr_A]);
+
+        if(!trans.mode_B)
+          Queue_B.push_back(old_read);
+      end
+
+      count++;
+
+      if(count==transaction_cnt+1) begin
+        verdict();
+
+        count=0;
+        error_cnt=0;
+        Queue_A.delete();
+        Queue_B.delete();
+
+        -> done;
+        break;
+      end
+    end
+
+  endtask
+
+  //Final design status after all tests
+  task verdict();
+
+    $display("\n================================");
+    $display("        DESIGN STATUS");
+    $display("================================");
+
+    if(error_cnt==0)
+      $display("DUAL PORT MEMORY PASSED");
+    else
+      $display("DUAL PORT MEMORY  FAILED");
+
+    $display("Total Errors = %0d",error_cnt);
+    $display("================================\n");
+
+  endtask
+
+endclass
+
+
 
