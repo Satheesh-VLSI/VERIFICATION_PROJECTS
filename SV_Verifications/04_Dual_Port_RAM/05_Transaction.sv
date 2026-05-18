8ea9d25b7f139772074d9ee8f1ac24a288133fa1
Satheesh <satheeshnarayan05@gmail.com>
2026-05-18 09:07:39 +0530

    Update 05_Transaction.sv

--- SV_Verifications/04_Dual_Port_RAM/05_Transaction.sv
+++ SV_Verifications/04_Dual_Port_RAM/05_Transaction.sv
@@ -1,1 +1,37 @@
 
+//============================
+//        TRANSACTION
+//============================
+class Transaction;
+
+  rand bit [7:0] data_in_A,data_in_B;
+  randc bit [5:0] addr_A,addr_B;
+  rand bit mode_A,mode_B;
+
+  bit [7:0] data_out_A,data_out_B;
+
+  constraint c1 {
+    mode_A dist {0:=4,1:=6};
+    mode_B dist {0:=4,1:=6};
+
+    (mode_A==0) -> data_in_A==0;
+    (mode_B==0) -> data_in_B==0;
+  }
+
+  function void display(string name);
+
+    $display("+------------------------------------------------+");
+    $display("|      %0s          |   Time  : %4t         | ",name,$time);
+    $display("+------------------------------------------------+");
+    $display("| Address A  :%4d      | Address B  :%4d       |",addr_A,addr_B);
+    $display("| Data In A  :%4d      | Data In B  :%4d       |",data_in_A,data_in_B);
+    $display("| Mode A     :   %b      | Mode B     :   %b       |",mode_A,mode_B);
+    $display("| Data Out A :%4d      | Data Out B :%4d       |",data_out_A,data_out_B);
+    $display("+------------------------------------------------+");
+
+  endfunction
+
+endclass
+
+
+
