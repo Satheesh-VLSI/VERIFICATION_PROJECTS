0abe99da18cc2a52d0f6e7d2da65326e9fd6fec6
Satheesh <satheeshnarayan05@gmail.com>
2026-05-23 15:44:42 +0530

    Update 10_Interface.sv

--- SV_Verifications/04_Dual_Port_RAM/10_Interface.sv
+++ SV_Verifications/04_Dual_Port_RAM/10_Interface.sv
@@ -12,16 +12,16 @@ interface RAM_INTERFACE(input logic clk);
   clocking cb @(posedge clk);
     default input #1step output #0;
     input data_out_A,data_out_B;
-    output data_in_A,data_in_B,addr_A,addr_B,mode_A,mode_B;
+    output data_in_A,data_in_B,addr_A,addr_B,mode_A,mode_B,rst;
   endclocking
   
   
   clocking cb1 @(posedge clk);
     default input #1step output #0;
     input data_out_A,data_out_B;
-    input data_in_A,data_in_B,addr_A,addr_B,mode_A,mode_B;
+    input data_in_A,data_in_B,addr_A,addr_B,mode_A,mode_B,rst;
   endclocking
 
-  modport DUT (input data_in_A,data_in_B,addr_A,addr_B,mode_A,mode_B,clk,output data_out_A,data_out_B);
-  modport TB  (clocking cb,clocking cb1,input data_out_A,data_out_B,data_in_A,data_in_B,addr_A,addr_B,mode_A,mode_B);
+  modport DUT (input data_in_A,data_in_B,addr_A,addr_B,mode_A,mode_B,rst,clk,output data_out_A,data_out_B);
+  modport TB  (clocking cb,clocking cb1,input data_out_A,data_out_B,data_in_A,data_in_B,addr_A,addr_B,mode_A,mode_B,rst);
 endinterface
