542506abdb3fb6b34018385abbe81ab35aec9241
Satheesh <satheeshnarayan05@gmail.com>
2026-05-23 18:38:49 +0530

    Update 02_Testbench_top.sv

--- SV_Verifications/03_Decade_Counter/Asynchronous_Counter_with_Asynchronus_Reset/02_Testbench_top.sv
+++ SV_Verifications/03_Decade_Counter/Asynchronous_Counter_with_Asynchronus_Reset/02_Testbench_top.sv
@@ -4,25 +4,29 @@
 //=======================
 module tb_top;
   logic clk;
-  initial clk=0;
-  always #5 clk=~clk;
+  initial clk = 0;
+  always #5 clk = ~clk;
 
   DECADE_CNT_INTERFACE inf(clk);
 
-  Decade dut(.clk(inf.clk),
-             .rst(inf.rst),
-             .Q(inf.q));
+  Decade dut(
+    .clk (inf.clk),
+    .rst (inf.rst),
+    .Q   (inf.q)
+  );
 
   Environment env;
 
   initial begin
     
-    env=new(inf);
+    $dumpfile("c.vcd");
+    $dumpvars;
+    env = new(inf);
     $display("==================================================================");
-    $display("    SYNC DECADE COUNTER WITH SYNC RESET VERIFICATION STARTED");
+    $display("    ASYNC DECADE COUNTER VERIFICATION STARTED");
     $display("==================================================================");
     env.test();
     #20;
     $finish;
   end
-endmodule:tb_top
+endmodule
