cb6a7cfd6e5fb3123b7ad757ba4149bdd2e4480f
Satheesh <satheeshnarayan05@gmail.com>
2026-05-23 18:38:38 +0530

    Update 03_Environment.sv

--- SV_Verifications/03_Decade_Counter/Asynchronous_Counter_with_Asynchronus_Reset/03_Environment.sv
+++ SV_Verifications/03_Decade_Counter/Asynchronous_Counter_with_Asynchronus_Reset/03_Environment.sv
@@ -1,31 +1,32 @@
+      
+    $display("Total Errors = %0d", error_cnt);
+    $display("===================================\n");
+  endtask
+endclass:Scoreboard
 
 //========================
 //      ENVIRONMENT
 //========================
 class Environment;
-  
   Generator  gen;
   Driver     drv;
   Monitor    mon;
   Scoreboard scr;
   
-  mailbox #(Transaction) gen2drv; //mailbox to send data from geenerator to driver
-  mailbox #(Transaction) mon2scr; //mailbox to send data from monitor to scoreboard
+  mailbox #(Transaction) gen2drv; 
+  mailbox #(Transaction) mon2scr; 
 
   function new(virtual DECADE_CNT_INTERFACE inf);
+    gen2drv = new();
+    mon2scr = new();
     
-    gen2drv=new();
-    mon2scr=new();
-    
-    gen=new(gen2drv);
-    drv=new(inf,gen2drv);
-    mon=new(inf,mon2scr);
-    scr=new(mon2scr);
-    
+    gen = new(gen2drv);
+    drv = new(inf, gen2drv);
+    mon = new(inf, mon2scr);
+    scr = new(mon2scr);
   endfunction
 
   task test();
-    
     fork
       gen.main();
       drv.main();
@@ -37,3 +38,6 @@ class Environment;
     disable fork;
   endtask
 endclass:Environment
+
+
+      
