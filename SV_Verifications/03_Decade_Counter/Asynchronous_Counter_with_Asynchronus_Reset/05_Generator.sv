20df26eca3dfb6976b39d1458c9464951ac19d9c
Satheesh <satheeshnarayan05@gmail.com>
2026-05-23 18:36:47 +0530

    Update 05_Generator.sv

--- SV_Verifications/03_Decade_Counter/Asynchronous_Counter_with_Asynchronus_Reset/05_Generator.sv
+++ SV_Verifications/03_Decade_Counter/Asynchronous_Counter_with_Asynchronus_Reset/05_Generator.sv
@@ -1,19 +1,18 @@
+
 //===========================
 //         GENERATOR
 //===========================
 class Generator;
-  
   Transaction trans;
   mailbox #(Transaction) gen2drv;
 
   function new(mailbox #(Transaction) gen2drv);
-    this.gen2drv=gen2drv; 
+    this.gen2drv = gen2drv;
   endfunction
 
   task main();
-    
     repeat(100) begin
-      trans=new();
+      trans = new();
       trans.randomize();
       gen2drv.put(trans);
     end
