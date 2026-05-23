42aeab64070f46e0b3fe9b0c736351fc5bc789f2
Satheesh <satheeshnarayan05@gmail.com>
2026-05-23 18:37:07 +0530

    Update 06_Driver.sv

--- SV_Verifications/03_Decade_Counter/Asynchronous_Counter_with_Asynchronus_Reset/06_Driver.sv
+++ SV_Verifications/03_Decade_Counter/Asynchronous_Counter_with_Asynchronus_Reset/06_Driver.sv
@@ -1,24 +1,31 @@
 //===========================
-//        DRIVER 
+//         DRIVER 
 //===========================
 class Driver;
-  
   virtual DECADE_CNT_INTERFACE inf;
   mailbox #(Transaction) gen2drv;
 
-  function new(virtual DECADE_CNT_INTERFACE inf,mailbox #(Transaction) gen2drv);
-    this.inf= inf;
-    this.gen2drv=gen2drv;
+  function new(virtual DECADE_CNT_INTERFACE inf, mailbox #(Transaction) gen2drv);
+    this.inf = inf;
+    this.gen2drv = gen2drv;
   endfunction
 
   task main();
-    inf.rst=1;
+    inf.rst = 1;
+    
     forever begin
       Transaction trans;
       gen2drv.get(trans);
 
       @(inf.cb);
-      inf.cb.rst<=trans.rst;
+      
+      
+      fork
+        begin
+          #($urandom_range(1,3)); 
+          inf.rst = trans.rst;
+        end
+      join_none
     end
   endtask
 endclass:Driver
