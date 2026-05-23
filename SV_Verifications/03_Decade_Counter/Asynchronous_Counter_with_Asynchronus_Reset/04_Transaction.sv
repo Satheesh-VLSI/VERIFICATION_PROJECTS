f430789384b86b2d06b3bf519d634bb0f6a31ac0
Satheesh <satheeshnarayan05@gmail.com>
2026-05-23 18:36:33 +0530

    Update 04_Transaction.sv

--- SV_Verifications/03_Decade_Counter/Asynchronous_Counter_with_Asynchronus_Reset/04_Transaction.sv
+++ SV_Verifications/03_Decade_Counter/Asynchronous_Counter_with_Asynchronus_Reset/04_Transaction.sv
@@ -5,9 +5,9 @@ class Transaction;
   rand bit rst;
   bit [3:0] q;
   
-  constraint rst_c{ rst dist {0:=11,1:=2};}
+  constraint rst_c{ rst dist {0:=11, 1:=2}; }
   
   function void display(string name);
-    $display("[%s] : rst=%0b q=%0d",name,rst,q);
+    $display("[%s] : rst=%0b q=%0d", name, rst, q);
   endfunction
 endclass:Transaction
