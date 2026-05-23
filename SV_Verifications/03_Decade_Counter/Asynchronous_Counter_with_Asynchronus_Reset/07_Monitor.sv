5722858693b500ae7f6c6b3a674750331d658de5
Satheesh <satheeshnarayan05@gmail.com>
2026-05-23 18:37:29 +0530

    Update 07_Monitor.sv

--- SV_Verifications/03_Decade_Counter/Asynchronous_Counter_with_Asynchronus_Reset/07_Monitor.sv
+++ SV_Verifications/03_Decade_Counter/Asynchronous_Counter_with_Asynchronus_Reset/07_Monitor.sv
@@ -1,30 +1,28 @@
 //==========================
-//        MONITOR 
+//         MONITOR 
 //==========================
 class Monitor;
-  
   virtual DECADE_CNT_INTERFACE inf;
   mailbox #(Transaction) mon2scr;
 
-  function new(virtual DECADE_CNT_INTERFACE inf,mailbox #(Transaction) mon2scr);
-    this.inf=inf;
-    this.mon2scr=mon2scr;
+  function new(virtual DECADE_CNT_INTERFACE inf, mailbox #(Transaction) mon2scr);
+    this.inf = inf;
+    this.mon2scr = mon2scr;
   endfunction
 
   task main();
     forever begin
-      
       Transaction trans;
-      trans=new();
+      trans = new();
 
-      @(inf.cb);
       
-      
-      trans.rst=inf.rst;
-      trans.q=inf.cb.q;
+      @(posedge inf.clk or posedge inf.rst);
+      #1; 
+
+      trans.rst = inf.rst;
+      trans.q   = inf.q;
 
       mon2scr.put(trans);
       trans.display("MONITOR");
     end
   endtask
-endclass:Monitor
