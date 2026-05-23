eb50b1d8630715cfa8ca80917ac41a8e93dd2a73
Satheesh <satheeshnarayan05@gmail.com>
2026-05-23 18:39:15 +0530

    Update 01_Design.v

--- SV_Verifications/03_Decade_Counter/Asynchronous_Counter_with_Asynchronus_Reset/01_Design.v
+++ SV_Verifications/03_Decade_Counter/Asynchronous_Counter_with_Asynchronus_Reset/01_Design.v
@@ -1,12 +1,44 @@
-//DUT
-module Decade (input clk,rst,output reg [3:0]Q);
+module Decade (
+    input clk,
+    input rst, 
+    output reg [3:0] Q
+);
+
+    reg self_clear;
+
   
-  always @(posedge clk) begin
-    if (rst)
-      Q<=4'b0000;
-    else if (Q==4'b1001)
-      Q<=4'b0000;
-    else
-      Q<=Q+1;
-  end
+  always @(negedge clk or posedge rst) 
+    self_clear = rst | (Q[3] & Q[0]); 
+  
+    always @(negedge clk or posedge self_clear) begin
+        if (self_clear) 
+            Q[0] <= 1'b0;
+        else            
+            Q[0] <= ~Q[0];
+    end
+
+  
+    always @(negedge Q[0] or posedge self_clear) begin
+        if (self_clear) 
+            Q[1] <= 1'b0;
+        else            
+            Q[1] <= ~Q[1];
+    end
+
+  
+    always @(negedge Q[1] or posedge self_clear) begin
+        if (self_clear) 
+            Q[2] <= 1'b0;
+        else            
+            Q[2] <= ~Q[2];
+    end
+
+  
+    always @(negedge Q[2] or posedge self_clear) begin
+        if (self_clear) 
+            Q[3] <= 1'b0;
+        else            
+            Q[3] <= ~Q[3];
+    end
+
 endmodule
