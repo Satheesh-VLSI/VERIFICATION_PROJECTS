ebd822a9f9a2c96bd5aabe983ce021ab90da8d89
Satheesh <satheeshnarayan05@gmail.com>
2026-05-30 18:12:19 +0530

    Update 08_Read_Driver.sv

--- SV_Verifications/05_FIFO/02_Asynchronous_FIFO/08_Read_Driver.sv
+++ SV_Verifications/05_FIFO/02_Asynchronous_FIFO/08_Read_Driver.sv
@@ -1,1 +1,40 @@
 
+
+    
+//=======================
+//     read  DRIVER
+//=======================
+class Read_Driver;
+
+  virtual FIFO_INTERFACE inf;
+  mailbox #(Transaction) gen2drv;
+
+  function new(
+    virtual FIFO_INTERFACE inf,
+    mailbox #(Transaction) gen2drv
+  );
+
+    this.inf=inf;
+    this.gen2drv=gen2drv;
+
+  endfunction
+
+  task main();
+
+    forever begin
+      Transaction trans;
+      gen2drv.get(trans);
+
+      @(inf.r_cb);
+
+      //addr and control signals
+      inf.r_cb.r_en<=trans.r_en;
+
+    end
+
+  endtask
+
+endclass
+
+    
+
