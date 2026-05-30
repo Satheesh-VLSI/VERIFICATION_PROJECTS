c94ddd56d8f00d0248e5913d43400c0a68717825
Satheesh <satheeshnarayan05@gmail.com>
2026-05-30 18:13:28 +0530

    Update 10_Read_Monitor.sv

--- SV_Verifications/05_FIFO/02_Asynchronous_FIFO/10_Read_Monitor.sv
+++ SV_Verifications/05_FIFO/02_Asynchronous_FIFO/10_Read_Monitor.sv
@@ -1,1 +1,42 @@
+//========================
+//     READ MONITOR 
+//========================
+class Read_Monitor;
 
+  virtual FIFO_INTERFACE inf;
+  mailbox #(Transaction) mon2scr;
+
+  function new(virtual FIFO_INTERFACE inf,
+               mailbox #(Transaction) mon2scr);
+
+    this.inf=inf;
+    this.mon2scr=mon2scr;
+
+  endfunction
+
+  task main();
+
+    forever begin
+      
+      Transaction trans;
+      trans=new();
+
+      @(inf.r_cb1);
+
+      //output signals from interface to monitor through clocking block
+      trans.data_out=inf.r_cb1.data_out;
+      trans.empty=inf.r_cb1.empty;
+
+      //input signals from interface to monitor through clocking block
+      
+      trans.r_en=inf.r_cb1.r_en;
+      trans.r_rst=inf.r_cb1.r_rst;
+
+      mon2scr.put(trans);
+//       trans.display_read("Read_MONITOR");
+
+    end
+
+  endtask
+
+endclass
