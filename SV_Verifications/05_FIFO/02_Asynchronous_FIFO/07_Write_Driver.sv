95295897316a5f9450d9b1b9f6a1cc7e1efcb319
Satheesh <satheeshnarayan05@gmail.com>
2026-05-30 18:11:57 +0530

    Update 07_Write_Driver.sv

--- SV_Verifications/05_FIFO/02_Asynchronous_FIFO/07_Write_Driver.sv
+++ SV_Verifications/05_FIFO/02_Asynchronous_FIFO/07_Write_Driver.sv
@@ -1,1 +1,41 @@
+    
+//=======================
+//     Write  DRIVER
+//=======================
+class Write_Driver;
+
+  virtual FIFO_INTERFACE inf;
+  mailbox #(Transaction) gen2drv;
+
+  function new(
+    virtual FIFO_INTERFACE inf,
+    mailbox #(Transaction) gen2drv
+  );
+
+    this.inf=inf;
+    this.gen2drv=gen2drv;
+
+  endfunction
+
+  task main();
+
+    forever begin
+      Transaction trans;
+      gen2drv.get(trans);
+
+      @(inf.w_cb);
+
+      //data signals
+      inf.w_cb.data_in<=trans.data_in;
+      
+      //addr and control signals
+      inf.w_cb.w_en<=trans.w_en;
+      
+      
+
+    end
+
+  endtask
+
+endclass
 
